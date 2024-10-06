68e8124aa40989d8230f4c4dbbb1281c8e060130
JasonWarner <9655826+jasonwarner@user.noreply.gitee.com>
2024-10-06 17:13:35 +0800

    L02_提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 
 
 public class Main {
-    public static void main(String[] args) {//TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello Java:CHEN XIA  ");
+    public static void main(String[] args) {
+
+        System.out.printf("Hello Java:CHEN XIA");
 
     }
 }
